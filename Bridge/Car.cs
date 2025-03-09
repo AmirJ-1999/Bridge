4a6a754cefe5e6418b81c7305f1a3b6072b4efa2
Amir Jawad <amirjawad79@gmail.com>
2025-03-09 17:56:00 +0100

    Created Car class with properties, return values, and summaries for documentation of the codes, which will later be exported to the xml file.

---
+++ Bridge/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// Represents a car vehicle with license plate and date properties.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// Gets or sets the cars license plate.
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date associated with the car.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Returns the fixed price for crossing the bridge.
+        /// </summary>
+        /// <returns>Fixed price of 230.</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// Returns the type of vehicle.
+        /// </summary>
+        /// <returns>Returns the string "Car".</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+    }
+}
